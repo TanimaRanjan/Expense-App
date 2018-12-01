52be1765ec7be2cc7869f7726763de966660ed53
Tanima Ranjan <tanimaranjan@gmail.com>
2018-11-30 19:57:26 -0500

    Expense Dashboard page

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
+import ExpenseList from './ExpenseList'
+import ExpenseListFilters from './ExpenseListFilters'
 
 const ExpenseDashBoardPage = () => (
-    <div>This is from my Dashboard Components</div>
+    <div>This is from my Dashboard Components
+        <ExpenseListFilters></ExpenseListFilters>
+        <ExpenseList></ExpenseList>
+    </div>
 )
 
 export default ExpenseDashBoardPage;
