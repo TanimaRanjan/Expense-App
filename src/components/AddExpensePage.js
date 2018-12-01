0f2fb3ebdb2332d9d352df1213820c860c28666e
Tanima Ranjan <tanimaranjan@gmail.com>
2018-11-30 19:56:08 -0500

    Add Expense Page

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
+import ExpenseForm from './ExpenseForm'
 
-const AddExpensePage = () => (
-    <div>Add Expense Page</div>
-)
+const AddExpensePage = (props) => {
+    return (
+    <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm></ExpenseForm>
+    </div>
+)}
 
 export default AddExpensePage;
