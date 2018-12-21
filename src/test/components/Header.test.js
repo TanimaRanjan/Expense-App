62de399966827c7474f6e2a934abd19edd232519
Tanima Ranjan <tanimaranjan@gmail.com>
2018-12-21 07:59:35 -0500

    Updates

--- src/test/components/Header.test.js
+++ src/test/components/Header.test.js
@@ -8,9 +8,9 @@ import toJSON from 'enzyme-to-json'
 test('Should render Header correctly ', () => {
     const wrapper = shallow(<Header />)
     expect(toJSON(wrapper)).toMatchSnapshot()
-    // expect(wrapper.find('h1').length).toBe(1)
-    // expect(wrapper.find('h1').text()).toBe("Expense App")
-    // const renender = new ReachShallowRenderer();
-    // renender.render(<Header />)
-    // expect(renender.getRenderOutput()).toMatchSnapshot()
+        // expect(wrapper.find('h1').length).toBe(1)
+        // expect(wrapper.find('h1').text()).toBe("Expense App")
+        // const renender = new ReachShallowRenderer();
+        // renender.render(<Header />)
+        // expect(renender.getRenderOutput()).toMatchSnapshot()
 })
