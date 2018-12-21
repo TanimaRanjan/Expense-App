f3fac9a07a0fb299e973185bf5d28aba8dabd173
Tanima Ranjan <tanimaranjan@gmail.com>
2018-12-21 08:09:50 -0500

    Updates

--- src/test/components/ExpenseList.test.js
+++ src/test/components/ExpenseList.test.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import  ExpenseList  from '../../components/ExpenseList'
 import expenses from '../fixtures/expenses'
+import toJSON from 'enzyme-to-json'
 
 test('Should render ExpenseList with expenses', () => {
     const wrapper = shallow(<ExpenseList expenses={expenses} />)
-    expenses(wrapper).toMatchSnapshot()
+    expenses(toJSON(wrapper)).toMatchSnapshot()
 })
 
 test('Should render Expense List with empty message' , () => {
