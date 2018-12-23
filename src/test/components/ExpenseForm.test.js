0475e86ebd032c85c4fdbc7cf7f7b53b5c57beb2
Tanima Ranjan <tanimaranjan@gmail.com>
2018-12-23 08:16:48 -0500

    updates

--- src/test/components/ExpenseForm.test.js
+++ src/test/components/ExpenseForm.test.js
@@ -14,3 +14,14 @@ test('Should render ExpenseForm with expense data', () => {
     const wrapper = shallow(<ExpenseForm expense={expense[1]} />)
     expect(wrapper).toMatchSnapshot()
 })
+
+
+test('Should render error for invalid for submission', ()=> {
+    const wrapper = shallow(<ExpenseForm  />)
+    expect(wrapper).toMatchSnapshot()
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    })
+    expect(wrapper.state('error').length).toBeGreaterThan(0)
+    expect(wrapper).toMatchSnapshot()
+})
