690d85ace462921ae3059c9070fa1cfe79911bbd
Tanima Ranjan <tanimaranjan@gmail.com>
2018-12-24 10:19:40 -0500

    Updates

---
+++ src/test/components/AddExpensePage.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import AddExpensePage from '../../components/AddExpensePage'
+
+test('Should render AddExpensePage properly', () => {
+    const onSubmit = jest.fn();
+    const history = { push : jest.fn()}
+    
+})
