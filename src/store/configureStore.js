08dda692edd7f92073c8013b9b0b312b6ca4592f
Tanima Ranjan <tanimaranjan@gmail.com>
2018-12-24 13:22:29 -0500

    Updates

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,10 +2,6 @@ import {createStore, combineReducers} from 'redux';
 import expensesReducer from '../reducers/expenses'
 import filtersReducer from '../reducers/filters'
 
-
-
-
-
 // Store creation
 export default () => {
     const store = createStore(
