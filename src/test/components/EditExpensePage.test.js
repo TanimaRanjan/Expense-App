6013151f78de1b4f681ea95b57748fa53ced1022
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-02 20:00:31 -0500

    Updates

--- src/test/components/EditExpensePage.test.js
+++ src/test/components/EditExpensePage.test.js
@@ -7,17 +7,17 @@ import expenses from '../fixtures/expenses'
 let editExpense, removeExpense, history, wrapper 
 
 beforeEach(() => {
-    const editExpense = jest.fn();
-    const removeExpense = jest.fn();
-    const history = { push : jest.fn()}
-    const wrapper = shallow(<EditExpensePage 
+    editExpense = jest.fn();
+    removeExpense = jest.fn();
+    history = { push : jest.fn()}
+    wrapper = shallow(<EditExpensePage 
             editExpense={editExpense} 
             removeExpense={removeExpense} 
             history={history} 
             expense={expenses[2]}/>)
 })
 test('Should render EditExpensePage properly', () => {
-    // expect(wrapper).toMatchSnapshot()
+    expect(wrapper).toMatchSnapshot()
 })
 
 test('Should render EditExpensePage properly', () => {
