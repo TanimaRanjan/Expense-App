8fa4fe302824f1f517510f9d2ef84929995139f7
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-02 19:35:38 -0500

    Updates

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,6 +3,20 @@ import { connect} from 'react-redux'
 import ExpenseForm from './ExpenseForm'
 import { editExpense , removeExpense} from '../actions/expenses';
 
+// refactor editexpese page to be a class based componenet
+// setup match 
+
+export class EditExpensePage extends React.Component {
+    onSubmit = ( expense ) => {
+        this.props.dispatch(editExpense(this.props.expense.id, expense))
+        this.props.history.push('/')
+    }
+
+    onRemove =  ( expense ) => {
+
+    }
+}
+
 const EditExpensePage = (props) =>  {
     console.log(props);
     return (
@@ -22,6 +36,14 @@ const EditExpensePage = (props) =>  {
     </div>
     )
 }
+
+const mapDistpatchToProp = () => {
+    return {
+        editExpense: 
+    }
+    
+}
+
 const mapStateToProp = (state, props) => {
     return { 
         expense: state.expenses.find((expense) => {
