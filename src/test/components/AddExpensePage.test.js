42e152e1e7d00b0f4c7c16b3d68932efffec26f5
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-02 19:37:22 -0500

    Updates

--- src/test/components/AddExpensePage.test.js
+++ src/test/components/AddExpensePage.test.js
@@ -3,19 +3,22 @@ import { shallow } from 'enzyme'
 import AddExpensePage from '../../components/AddExpensePage'
 import expenses from '../fixtures/expenses'
 
+
+
+let addExpense, history, wrapper 
+
+beforeEach(() => {
+    const onSubmit = jest.fn();
+    const history = { push : jest.fn()}
+    // const wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />)
+})
 test('Should render AddExpensePage properly', () => {
-    // const onSubmit = jest.fn();
-    // const history = { push : jest.fn()}
-    // const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
     // expect(wrapper).toMatchSnapshot()
 })
 
 
 test('Should handle OnSubmit ', () => {
-    // const onSubmit = jest.fn();
-    // const history = { push : jest.fn()}
-    // const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
     // wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
     // expect(history.push).toHaveBeenLastCalledWith('/')
-    // expect(onSubmit).toHaveBeenLastCalledWith(expenses[1])
+    // expect(addExpense).toHaveBeenLastCalledWith(expenses[1])
 })
