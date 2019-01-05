32b4b17848b3a322012b704369f63e92d1a7ca51
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-05 15:52:32 -0500

    More test cases

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -1,10 +1,11 @@
+import 'react-dates/initialize';
 import React from 'react'
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
 import ExpenseList from './ExpenseList'
 import ExpenseListFilters from './ExpenseListFilters'
 
 const ExpenseDashBoardPage = () => (
-    <div>
+    <div>This is from my Dashboard Components
         <ExpenseListFilters></ExpenseListFilters>
         <ExpenseList></ExpenseList>
     </div>
