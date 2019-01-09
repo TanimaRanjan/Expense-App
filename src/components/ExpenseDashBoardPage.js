6e829f8a2fe97c41aae757eab3345120f1b9b3d2
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-08 19:51:03 -0500

    Dashboard update

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -3,9 +3,11 @@ import React from 'react'
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
 import ExpenseList from './ExpenseList'
 import ExpenseListFilters from './ExpenseListFilters'
+import ExpensesSummary from './ExpensesSummary'
 
 const ExpenseDashBoardPage = () => (
     <div>
+        <ExpensesSummary />
         <ExpenseListFilters></ExpenseListFilters>
         <ExpenseList></ExpenseList>
     </div>
