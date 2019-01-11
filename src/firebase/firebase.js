048b6a6a8cf26f7a74381140a6945c159fb98d5f
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-10 19:13:39 -0500

    Update for database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,20 +11,71 @@ const config = {
   }
   firebase.initializeApp(config)
 
-  firebase.database().ref().set({
-      name: 'Tanima Ranjan',
-      age: 36,
-      isSingle: false,
-      location : {
-          city: 'New York',
-          country: 'United States'
-      }
-  })
+const database = firebase.database()
 
-  firebase.database().ref('age').set(35)
-  firebase.database().ref('location/city').set('Brooklyn')
+//   database.ref().set({
+//       name: 'Tanima Ranjan',
+//       age: 36,
+//       stressLevel:6,
+//       job: {
+//             title:'Software Devloper',
+//             company:'Google'
+//       },
+//       location : {
+//           city: 'New York',
+//           country: 'United States'
+//       }
+//   }).then(() => {
+//       console.log('Data is saved')
+//   }).catch((error) => {
+//         console.log('This failed ', error)
+//   })
 
-  firebase.database().ref('attributes').set({
-      height:5.1,
-      weight:133
-  })
+// //   firebase.database().ref('age').set(35)
+// //   firebase.database().ref('location/city').set('Brooklyn')
+
+//   firebase.database().ref('attributes').set({
+//       height:5.1,
+//       weight:133
+//   }).then(() => {
+//     console.log('Added Attributes ')
+//   }).catch((e) => {
+//     console.log('Error - ',e)
+//   })
+// //   console.log('I made a request to change the data')
+
+// database.ref('isSingle')
+// .remove()
+// .then(() => {
+//     console.log('Data was removed')
+// }).catch(() => {
+//     console.log('Did not remove data ', e)
+// })
+
+
+// database.ref('isSingle').set(null)
+
+// database.ref().update({
+//     name: 'Tanima Romero',
+//     age: 35,
+//     job: 'Software Developer', 
+//     isSingle:null,
+//     'location/city': 'Brooklyn'
+// }).then(() => {
+//     console.log('Data has been updated')
+// }).catch(() => {
+//     console.log('Did not update the data ', e)
+// })
+
+
+// database.ref().update({
+//     stressLevel:9,
+//     'job/C'
+// })
+
+database.ref().update({
+    name: 'Tanima Romero',
+    stressLevel: 9,
+    'job/company': 'Amazon',
+    'location/city':'Brooklyn'
+})
