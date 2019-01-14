96b7a242753b3c30c10adc8e6bc57b65c4125f85
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-14 16:46:35 -0500

    Environmental variable

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 // Accessing the path library using require(path)
 const path = require('path') 
+const webpack = require('webpack')
 
 // Webpack plugin to extract CSS
 // const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+if(process.env.NODE_ENV === 'test') {
+    require('dotenv').config({path : '.env.test'})
+} else if(process.env.NODE_ENV === 'development') {
+    require('dotenv').config({path : '.env.development'})
+}
 
 module.exports = (env) => {
     const isProduction = env === "production"
@@ -50,14 +59,21 @@ module.exports = (env) => {
                         options: {
                             sourceMap:true
                         }
-
                     }
                 ]
                 }
         ]
         },
         plugins : [
-            CSSExtract
+            CSSExtract, 
+            new webpack.DefinePlugin( { 
+                'process.env.FIREBASE_API_KEY' : JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL' : JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID' : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET' : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER' : JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER)
+            })
         ],
 
         devServer: {
