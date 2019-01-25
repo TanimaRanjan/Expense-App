09ff84a3a10c341a39290964304d711bc28eca56
Tanima Ranjan <tanimaranjan@gmail.com>
2019-01-24 19:28:45 -0500

    Styling

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,16 +5,25 @@ import selectExpenses from '../selectors/expense'
 // Stateless Function 
 export const ExpenseList = (props) => (
 
-        <div>
+        <div className='content-container'>
+            <div className='list-header'>
+                <div className='show-for-mobile'>Expenses</div>
+                <div className='show-for-desktop'>Expense</div>
+                <div className='show-for-desktop'>Amount</div>
+            </div>
+            <div className='list-body'>
             {
                 props.expenses.length === 0 ? (
-                    <p>No Expenses to show</p>
+                    <div className='list-item list-item__message'>
+                        <span>No Expenses</span>
+                    </div>
                 ) : (
                     props.expenses.map((expense) => {
                         return <ExpenseListItem key={expense.id} {...expense} /> 
                     })
                 )
             }
+            </div>
         </div>
 )
 
