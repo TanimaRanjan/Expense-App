e7f0f70f532ca203540f85dce8b6a5e03b52925a
Tanima Ranjan <tanimaranjan@gmail.com>
2019-02-06 20:14:50 -0500

    Clean up

--- src/firebase/firebase.1.js
+++ src/firebase/firebase.1.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase'
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyCo4OALSbUNcm27ghVNUNWArmInwPReHXU",
-    authDomain: "expense-app-f0a1f.firebaseapp.com",
-    databaseURL: "https://expense-app-f0a1f.firebaseio.com",
-    projectId: "expense-app-f0a1f",
-    storageBucket: "expense-app-f0a1f.appspot.com",
-    messagingSenderId: "365450292888"
-}
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER
+  }
 firebase.initializeApp(config)
 
 const database = firebase.database()
