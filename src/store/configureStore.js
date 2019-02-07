714985459f7ec53bfd7f33198602e4c0d476b951
Tanima Ranjan <tanimaranjan@gmail.com>
2019-02-07 17:45:56 -0500

    Format

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
-
-import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import expensesReducer from '../reducers/expenses'
 import filtersReducer from '../reducers/filters'
 import authReducer from '../reducers/auth'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = 
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ 
+        || compose
 
 // Store creation
 export default () => {
@@ -16,7 +17,6 @@ export default () => {
                 auth: authReducer
             }),
             composeEnhancers(applyMiddleware(thunk))
-            // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
         ) 
         return store;
 }
