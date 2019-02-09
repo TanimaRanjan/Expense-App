2ad17b34e18198718fda79632aa1b5609f3cde37
Tanima Ranjan <tanimaranjan@gmail.com>
2019-02-08 19:37:08 -0500

    Format

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,16 +3,13 @@ import { connect} from 'react-redux'
 import ExpenseForm from './ExpenseForm'
 import { startEditExpense , startRemoveExpense} from '../actions/expenses';
 
-// refactor editexpese page to be a class based componenet
-// setup match 
-
 export class EditExpensePage extends React.Component {
     onSubmit = ( expense ) => {
         this.props.startEditExpense(this.props.expense.id, expense)
         this.props.history.push('/dashboard')
     }
 
-    onRemove =  (  ) => {
+    onRemove =  () => {
         this.props.startRemoveExpense({id: this.props.expense.id})
         this.props.history.push('/dashboard')
     }
